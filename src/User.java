cc470c065027ea8debec50129666877b71dda91d
Jeneliya12 <gurungjeneliya@gmail.com>
2024-09-22 18:54:25 -0400

    add abstract User class with common attributes and constructor

---
+++ src/User.java
@@ -0,0 +1,13 @@
+public class User {
+    protected int id;
+    protected String name;
+    protected String email;
+    protected String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
