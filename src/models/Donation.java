f10fa95c03d7f53f2a46e0acecafe7abdfaf783c
Jeneliya12 <gurungjeneliya@gmail.com>
2024-09-22 19:15:35 -0400

    add Donation class with properties and constructor for managing food donations

---
+++ src/models/Donation.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Donation {
+    private int donationId;
+    private String foodType;
+    private double quantity;
+    private String unit;
+    private LocalDate expirationDate;
+    private boolean isClaimed;
+
+    public Donation(int donationId, String foodType, double quantity, String unit, LocalDate expirationDate) {
+        this.donationId = donationId;
+        this.foodType = foodType;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.expirationDate = foodexpirationDate;
+        this.isClaimed = false;
+    }
+
+}
